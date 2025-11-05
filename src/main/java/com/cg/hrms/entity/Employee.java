76273ba041c727a24ad0af2515e1b1be2ca7868a
satyap-02 <satyaprakash.vatsya@capgemini.com>
2025-11-05 17:21:51 +0530

    Employee.java

--- src/main/java/com/cg/hrms/entity/Employee.java
+++ src/main/java/com/cg/hrms/entity/Employee.java
@@ -1,13 +1,11 @@
-
 package com.cg.practicedb.entity;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import jakarta.persistence.*;
 
 @Entity
 @Table(name = "employees")
 public class Employee {
+
     @Id
     @Column(name = "employee_id")
     private Long id;
@@ -15,20 +13,29 @@ public class Employee {
     @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
     @ManyToOne
     @JsonBackReference
     @JoinColumn(name = "department_id")
-
     private Department department;
 
-    // Getters and Setters
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
     public String getFirstName() { return firstName; }
     public void setFirstName(String firstName) { this.firstName = firstName; }
 
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
     public Department getDepartment() { return department; }
     public void setDepartment(Department department) { this.department = department; }
 }
-
